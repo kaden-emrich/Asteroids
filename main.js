c1e44b7e7604ba9e3338dce34a54a242b5351b74
kaden-emrich <kadenemrich@gmail.com>
2023-05-05 07:50:22 -0400

    v0.2.2

--- main.js
+++ main.js
@@ -22,6 +22,8 @@ var acceleration = 0.2;
 var trippyMode = false;
 var showVelocity = false;
 var laserSight = false;
+var showBoundingBoxes = true;
+var boundingBoxColor = "#0ff";
 
 /*----- Game Settings End -----*/
 /*----- Classes -----*/
@@ -49,6 +51,12 @@ class PolarPoint {
         this.r = r;
         this.dir = dir;
     }// constructor(r, dir)
+
+    getRect() {
+        var x = this.r * Math.cos(this.dir * Math.PI / 180);
+        var y = this.r * Math.sin(this.dir * Math.PI / 180);
+        return new PointValue(x, y);
+    }// getRect()
 }// PolarPoint
 
 class Shape {
@@ -77,16 +85,72 @@ class Shape {
 }// Shape
 
 class Entity {
-    constructor(shape, color) {
+    constructor(shape, color, type) {
         this.x = canvas.width/2;
         this.y = canvas.height/2;
         this.dir = 0;
         this.xSpeed = 0;
         this.ySpeed = 0;
         this.color = color;
+        this.type = type;
+
+        this.index = entities.length;
+        entities[this.index] = this;
 
         this.shape = shape;
     }// constructor()
+
+    getPolarPoints() {
+        var pPoints = [];
+        for(let i = 0; i < this.shape.points.length; i++) {
+            var r = this.shape.points[i].r;
+            var dir = this.shape.points[i].dir + this.dir;
+            pPoints[i] = new PolarPoint(r, dir);
+        }
+        return pPoints;
+    }// getPolarPoints()
+
+    getPoints() {
+        var pPoints = this.getPolarPoints();
+        var rPoints = [];
+        for(let i = 0; i < pPoints.length; i++) {
+            var x = this.x + pPoints[i].r * Math.cos(pPoints[i].dir * Math.PI / 180);
+            var y = this.y + pPoints[i].r * Math.sin(pPoints[i].dir * Math.PI / 180);
+
+            rPoints[i] = new PointValue(x, y);
+        }
+
+        return rPoints;
+    }// getRectPoints()
+
+    getBoundingBox() {
+        var points = this.getPoints();
+
+        var xMin = points[0].x;
+        var xMax = points[0].x;
+        var yMin = points[0].y;
+        var yMax = points[0].y;
+
+        for(let i = 1; i < points.length; i++) {
+            if(points[i].x < xMin)
+                xMin = points[i].x;
+            if(points[i].x > xMax)
+                xMax = points[i].x;
+            if(points[i].y < yMin)
+                yMin = points[i].y;
+            if(points[i].y > yMax)
+                yMax = points[i].y;
+        }
+
+        var bBox = [
+            new PointValue(xMin, yMin),
+            new PointValue(xMax, yMin),
+            new PointValue(xMax, yMax),
+            new PointValue(xMin, yMax)
+        ];
+
+        return bBox;
+    }// getBoundingBox()
 
     getAbsSpeed() {
         return Math.sqrt(Math.pow(this.xSpeed, 2) + Math.pow(this.ySpeed, 2));
@@ -158,6 +222,39 @@ class Entity {
             ctx.stroke();
         }
     }// draw()*/
+
+    drawBoundingBox() {
+        var bBox = this.getBoundingBox();
+
+        ctx.strokeStyle = boundingBoxColor;
+        ctx.beginPath();
+        ctx.moveTo(bBox[0].x, bBox[0].y);
+        for(let i = 1; i < bBox.length; i++) {
+            ctx.lineTo(bBox[i].x, bBox[i].y);   
+        }
+        ctx.closePath();
+        ctx.stroke();
+    }// drawBoundingBox()
+
+    checkColision() {
+        for(let i = 0; i < entities.length; i++) {
+            if(entities[i] != null && i != this.index) {
+                var objectA = this.getBoundingBox();
+                var objectB = entities[i].getBoundingBox();
+
+                if(
+                    objectA[1].x >= objectB[0].x &&
+                    objectA[0].x <= objectB[1].x &&
+                    objectA[1].y >= objectB[0].y &&
+                    objectA[0].y <= objectB[1].y
+                ) {
+                    return entities[i].type;
+                }
+            }
+        }
+
+        return "none";
+    }// checkColision()
 }// Entity
 
 /*----- Classes End -----*/
@@ -170,18 +267,23 @@ var shipPoints = [
 ];
 
 var laserPoints = [
-    new PointValue(0, 1).getPolar(),
-    new PointValue(-10, 1).getPolar(),
-    new PointValue(-10, -1).getPolar(),
+    new PointValue(0, 0).getPolar(),
+    new PointValue(-30, 0).getPolar(),
+    new PointValue(-30, -1).getPolar(),
     new PointValue(0, -1).getPolar()
 ];
 
 /*----- Other Things End -----*/
 
-var ship = new Entity(new Shape(shipPoints), "#fff");
-entities[0] = ship;
+var ship = new Entity(new Shape(shipPoints), "#fff", "ship");
 ship.dir = 270;
 ship.draw = function() {
+    if(this.checkColision != "none") {
+        this.color = "#f00";
+    }
+    else {
+        this.color = "#fff";
+    }
     this.shape.draw(this.x, this.y, this.dir, this.color);
 
     if(showVelocity) {
@@ -208,21 +310,60 @@ ship.draw = function() {
 }// ship.draw()
 
 function shoot() {
-    console.log("shoot");
-    var laser = new Entity(new Shape(laserPoints), "#f00");
-    var index = entities.length;
-    entities[index] = laser;
+    //console.log("shoot"); // for debugging
+    var laser = new Entity(new Shape(laserPoints), "#f00", "laser");
+    laser.updatePosition = function() {
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+
+        
+        // off screen delete
+        if(this.x < -30  ||  this.x > canvas.width + 30  ||  this.y < -30  || this.y > canvas.height + 30) {
+            entities[laser.index] = null;
+        }
+    }
 
     laser.dir = ship.dir;
     laser.x = ship.x;
     laser.y = ship.y;
-    console.log("laser created");
+    //console.log("laser created"); // for debugging
 
     laser.forward(20);
 
-    console.log("laser shot");
+    //console.log("laser shot"); // for debugging
 
 }// shoot()
+
+function spawnAstroid(x, y, dir, size) {
+    // randomly generate astroid shape
+    var astroidPoints = [];
+    var min = 60;
+    var dif = 30;
+    
+    if(size == 3) {
+        min = 60;
+        dif = 30;
+    }
+    else if(size == 2) {
+        min = 40;
+        dif = 15;
+    }
+    else if(size == 1) {
+        min = 20;
+        dif = 10;
+    }
+
+    for(let i = 0; i < 18; i++) {
+        astroidPoints[i] = new PolarPoint(Math.random() * dif + min, i * 20);
+    }
+
+    var astroid = new Entity(new Shape(astroidPoints), "#fff", "astroid");
+    astroid.x = x;
+    astroid.y = y;
+    astroid.dir = dir;
+
+    astroid.forward(2);
+}// spawnAstroid()
 
 /*----- I/O -----*/
 
@@ -315,6 +456,10 @@ function drawEntities() {
     for(let i = 0; i < entities.length; i++) {
         if(entities[i] != null) {
             entities[i].draw();
+
+            if(showBoundingBoxes) {
+                entities[i].drawBoundingBox();
+            }
         }
     }
 }// drawEntities()
